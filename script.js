9253ac0ca11081f984160f1028df3d37d28ead43
Alexwebcreator <Sanek72Tob@mail.ru>
2021-11-25 19:23:36 +0500

    добавил переменные в script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const a = 1;
-let b = "вторая переменная";
+let title = 1;
+let screens = 2;
+let screenPrice = 3;
+let rollback = 4;
+let fullPrice = 5;
+let adaptive = 6;
+
+alert("Example alert");
+
+console.log("Hello, everyone!");
